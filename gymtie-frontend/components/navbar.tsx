285a2c749c77cc658ef00a5a1daa16ae1528f0ff
Syamlal CM <syamjayaraj@gmail.com>
2023-10-23 18:50:22 +0530

    logo, mobile view fixes

--- gymtie-frontend/components/navbar.tsx
+++ gymtie-frontend/components/navbar.tsx
@@ -19,42 +19,24 @@ export default function Navbar() {
 
   return (
     <nav className="navbar navbar-expand navbar-light bg-white topbar mb-4 static-top shadow">
-      <ul className="navbar-nav ml-auto">
-        <li className="nav-item dropdown no-arrow d-sm-none">
-          <a
-            className="nav-link dropdown-toggle"
-            href="#"
-            id="searchDropdown"
-            role="button"
-            data-toggle="dropdown"
-            aria-haspopup="true"
-            aria-expanded="false"
-          >
-            <i className="fas fa-search fa-fw"></i>
-          </a>
-          <div
-            className="dropdown-menu dropdown-menu-right p-3 shadow animated--grow-in"
-            aria-labelledby="searchDropdown"
-          >
-            <form className="form-inline mr-auto w-100 navbar-search">
-              <div className="input-group">
-                <input
-                  type="text"
-                  className="form-control bg-light border-0 small"
-                  placeholder="Search for..."
-                  aria-label="Search"
-                  aria-describedby="basic-addon2"
-                />
-                <div className="input-group-append">
-                  <button className="btn btn-primary" type="button">
-                    <i className="fas fa-search fa-sm"></i>
-                  </button>
-                </div>
+      <ul className="navbar-nav">
+        <li className="nav-item no-arrow">
+          <div className="row nav-left-container">
+            <div className="logo-container">
+              <Link className="nav-link" href="/">
+                <img src="/assets/images/logo.png" className="logo" />
+              </Link>
+            </div>
+            <div className="name-container">
+              <div className="gym-name">New Wings Health Club</div>
+              <div className="powered-by">
+                Powered by <a href="https://gymtie.com">GymTie</a>
               </div>
-            </form>
+            </div>
           </div>
         </li>
-
+      </ul>
+      <ul className="navbar-nav ml-auto">
         <li className="nav-item no-arrow">
           <Link href="/" className={`nav-link ${path === "/" ? "active" : ""}`}>
             Dashboard
